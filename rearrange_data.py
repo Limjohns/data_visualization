956ccf35009d597d4a087d8dd05dd1237190674c
Vegetable-Chicken-Coder <1512374909@qq.com>
2023-05-15 00:41:05 +0800

    update /data/data_13_22.csv

--- rearrange_data.py
+++ rearrange_data.py
@@ -35,12 +35,15 @@ f.close()
 #%% 整合所有年份电影数据到一个.csv文件中（不含票房等统计数据）
 base_df = pd.read_excel(os.path.join(raw_data_path, origin_data_path), sheet_name='2013').iloc[:-2]
 base_df = base_df.drop(labels=range(base_df.shape[0]))
+base_df = base_df.drop(labels='Release', axis=1) # 删去具体发布时间列，仅留下发布年份
+base_df = base_df.drop(labels='Rank', axis=1)
 for sheet_name in range(2013, 2023):
     df_tmp = pd.read_excel(os.path.join(raw_data_path, origin_data_path), sheet_name=str(sheet_name)).iloc[:-2]
     df_tmp = df_tmp.dropna()
     df_tmp['rank'] = [i + 1 for i in range(df_tmp.shape[0])]
     df_tmp['releaseYear'] = [sheet_name] * df_tmp.shape[0]
     df_tmp = df_tmp.drop(labels='Release', axis=1) # 删去具体发布时间列，仅留下发布年份
+    df_tmp = df_tmp.drop(labels='Rank', axis=1)
     df_tmp = df_tmp.iloc[:201] # 每年仅取前201个电影
     base_df = pd.concat([base_df, df_tmp], ignore_index = True)
     print(sheet_name, df_tmp.shape[0])
